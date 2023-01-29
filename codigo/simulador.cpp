196dc8b32940095a026a12eb3b64b13452e2204d
Edson Sales <edsondesouzasales@gmail.com>
2023-01-29 19:28:10 -0300

    feat/Adiciona rota POST com body e param

--- codigo/simulador.cpp
+++ codigo/simulador.cpp
@@ -1,11 +1,22 @@
+#include <string>
+#include <iostream>
 #include "CamadaFisica.cpp"
 #include "crow.h"
+#include "crow/middlewares/cors.h"
 
 int main() {
     AplicacaoTransmissora();
-    crow::SimpleApp app;
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    crow::App<crow::CORSHandler> app;
+
+    auto& cors = app.get_middleware<crow::CORSHandler>();
+
+    cors.global().origin("*");
+
+    CROW_ROUTE(app, "/<int>").methods("POST"_method)([](const crow::request& req, crow::response& res, int id) {
+        cout<< req.body.data() << endl;
+        res.write("oi"+std::to_string(id));
+        res.end();
     });
+
     app.port(18080).multithreaded().run();
 }
