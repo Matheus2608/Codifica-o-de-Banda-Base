92bfdffa1f11fc28677106a83ef0b3176691876e
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2023-02-04 19:00:48 -0300

    Refatoracao total do metodo de contagem de caracteres Metodo estava errado Refatorei os testes e com isso consegui fazer as funcoes funcionarem como deviam

--- codigo/enlace/testa_enlace.cpp
+++ codigo/enlace/testa_enlace.cpp
@@ -3,52 +3,73 @@
 #include "../catch.hpp"
 
 vi TestaCamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(vi quadro, int tamMaxQuadro) {
-    int numQuadrosPerfeitos = quadro.size() / tamMaxQuadro;
-    cout << numQuadrosPerfeitos << endl;
-    vi resultadoEnquadramento;
+    cout << "----------------------------------------------------------------" << endl;
+    cout << endl;
     bitset<8> tamMaxEmByte(tamMaxQuadro);
+    int tamCargaUtil = tamMaxQuadro - 1;
+
+    int numBytes = quadro.size() / 8;
+
+    cout << "numBytes = " << numBytes << endl;
+
+    int numQuadrosPerfeitos = numBytes / tamCargaUtil;
+
+    cout << "numQuadrosPerfeitos = " << numQuadrosPerfeitos << endl;
+    vi resultadoEnquadramento;
 
     for(int i = 0; i < numQuadrosPerfeitos; i++) {
         for(int j = 7; j >= 0; j--){
             resultadoEnquadramento.pb(tamMaxEmByte[j]);
         }
 
-        for(int k = 0; k < tamMaxQuadro; k++) {
-            resultadoEnquadramento.pb(quadro[i*tamMaxQuadro+k]);
+        for(int k = 0; k < tamCargaUtil * 8; k++) {
+            resultadoEnquadramento.pb(quadro[i*8+k]);
         }
     }
 
-    int numBitsFaltantes = quadro.size() - (numQuadrosPerfeitos * tamMaxQuadro);
-    if(numBitsFaltantes) {
-        cout << numBitsFaltantes << endl;
-        bitset<8> numBitsFaltantesEmByte(numBitsFaltantes);
+    int numBytesFaltantes = numBytes % tamCargaUtil;
+    if(numBytesFaltantes) {
+        cout << "numBytesFaltantes = " << numBytesFaltantes << endl;
+        bitset<8> numBytesFaltantesEmByte(numBytesFaltantes+1);
 
-        for(int j = 7; j >= 0; j--){
-            resultadoEnquadramento.pb(numBitsFaltantesEmByte[j]);
+        for(int i = 7; i >= 0; i--){
+            resultadoEnquadramento.pb(numBytesFaltantesEmByte[i]);
         }
 
-        for(int z = numQuadrosPerfeitos * tamMaxQuadro; z < quadro.size(); z++){
-            resultadoEnquadramento.pb(quadro[z]);
+        for(int j = numQuadrosPerfeitos * tamCargaUtil * 8; j < quadro.size(); j++){
+            resultadoEnquadramento.pb(quadro[j]);
         }
     }
 
     return resultadoEnquadramento;
 }
 
+// vi TestaCamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(vi quadro) {
+//     for(int i = 0; i < quadro.size(); i++) {
+//         for(int j = 0; i < )
+//     }
+// }
+
 TEST_CASE( " Testa CamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres com todos quadros completos") {
     vi input = {0,1,1,0,0,0,0,1};
-    int tamMaxQuadro = 4;
+    vi expected_output = {0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,1};
+    int tamMaxQuadro = 2;
 
-    vi expected_output = {0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,1};
+    REQUIRE(expected_output == TestaCamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(input, tamMaxQuadro));
+
+    input = {0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0};
+    expected_output = {0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,1,1,1,0,0};
+    tamMaxQuadro = 2;
 
     REQUIRE(expected_output == TestaCamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(input, tamMaxQuadro));
 }
 
 TEST_CASE( " Testa CamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres com um quadro incompleto") {
+    vi input = {0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0,0,0,0,1,1,0,1,0};
+    vi expected_output = {0,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,1,0};
+    int tamMaxQuadro = 3;
 
-    input = {0,1,1,0,0,0,0,1,1};
-    tamMaxQuadro = 4;
-
-    expected_output = {0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1};
     REQUIRE(expected_output == TestaCamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(input, tamMaxQuadro));
+
 }
+
