f967876c75a51dd82e54a7a4329853a662b3a89c
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2023-02-04 21:28:07 -0300

    desenquadramento, testes passando

--- codigo/enlace-receptora/CamadaEnlace.cpp
+++ codigo/enlace-receptora/CamadaEnlace.cpp
@@ -1,5 +1,39 @@
 #include "CamadaEnlace.hpp"
 
-/*
-    Funções aqui
-*/
+vi CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(vi quadro) {
+    bool cabecalho = true;
+    int i = 0, tamanhoCargaUtil;
+    vi output;
+
+    while(i < quadro.size()){
+        if(cabecalho){
+            // cout << "estou no if" << endl;
+            // cout << "i = " << i << endl;
+
+            bitset<8> bitsetCabecalho;
+            for(int j = i; j < i + 8; j++){
+                bitsetCabecalho[7-(j%8)] = quadro[j];
+                //cout << " bitsetCabecalho[" << 7-(j%8) << "] = " << bitsetCabecalho[7-j] << endl;
+            }
+
+            tamanhoCargaUtil = (int) (bitsetCabecalho.to_ullong() - 1);
+            cabecalho = false;
+            // cout << "tamanhoCargaUtil = " << tamanhoCargaUtil << endl;
+            i += 8;
+        }
+
+        else{
+            // cout << "estou no else" << endl;
+            // cout << "i = " << i << endl;
+
+            for(int k = i; k < i + (8 * tamanhoCargaUtil); k++){
+                output.pb(quadro[k]);
+            }
+
+            i += (8 * tamanhoCargaUtil);
+            cabecalho = true;
+        }
+    }
+
+    return output;
+}
--- codigo/enlace-receptora/TestaCamadaEnlace.cpp
+++ codigo/enlace-receptora/TestaCamadaEnlace.cpp
@@ -1,7 +1,22 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../catch.hpp"
-#include "CamadaEnlace.hpp"
+#include "CamadaEnlace.cpp"
 
-/*
-    Testes Aqui
-*/
+TEST_CASE( " Testa CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres com todos quadros completos") {
+    vi input = {0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,1};
+    vi expected_output = {0,1,1,0,0,0,0,1};
+
+    REQUIRE(expected_output == CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(input));
+
+    input = {0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,1,1,1,0,0};
+    expected_output = {0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0};
+
+    REQUIRE(expected_output == CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(input));
+}
+
+TEST_CASE( " Testa CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres com um quadro incompleto") {
+    vi input = {0,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,1,0};
+    vi expected_output = {0,1,1,0,0,0,0,1,0,1,0,1,1,1,0,0,0,0,0,1,1,0,1,0};
+
+    REQUIRE(expected_output == CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(input));
+}
